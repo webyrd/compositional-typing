a92cef6bc2fdd3f94d26b4fa9466b0d873575d21
William Byrd <webyrd@gmail.com>
2018-07-22 13:48:45 -0500

    working on tests

--- simple-typed.rkt
+++ simple-typed.rkt
@@ -3154,6 +3154,8 @@
 |#
 
 
+;; stutter example taken from 'Type-and-Example-Directed Program Synthesis' by
+;; Peter-Michael and Osera Steve Zdancewic (PLDI '15)
 (time
   (test "stutter-!-o-with-stutter-1"
     (run 1 (q)
